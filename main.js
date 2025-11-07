6fb60f4da23f1389af3083961ee55bee96b7dc63
simonestrinati <simone.strinati@outlook.it>
2025-11-07 03:10:38 +0100

    (pariEdispari) creazione seconda funzione

--- main.js
+++ main.js
@@ -5,14 +5,28 @@
  */
 
 let puntata = prompt("Scegli tra pari o dispari")
-let puntataGiocatore = parseInt(prompt("Scegli tra pari o dispari"))
+let puntataGiocatore = parseInt(prompt("Scegli un numero tra 0 e 5"))
 
 /**
  * Funzione per generare numeri randomici e creazione variabile puntataComputer
  */
 
-function numeroRandom (min, max) {
+function numeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) +1);   
 }
 
-let puntataComputer = numeroRandom (0, 5);
+let puntataComputer = numeroRandom(0, 5);
+
+//console.log("puntata su:", puntata);
+//console.log("Il giocatore punta su:", puntataGiocatore);
+//console.log("Il computer punta su:", puntataComputer);
+
+/**
+ * Creazione seconda funzione per contollare se numero è pari o dispari
+ */
+
+function pariOdispari(numero) {
+     return numero % 2 != 0;
+}
+
+pariOdispari(puntataGiocatore + puntataComputer);
